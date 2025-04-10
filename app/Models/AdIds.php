85a09a8c8529e4a389d6fa5b042c500385facafa
Krushnkant Vadadoriya <krushnvadadoriya@gmail.com>
2025-04-10 14:24:21 +0530

    First Push

---
+++ app/Models/AdIds.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdIds extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_ad_ids';
+}
